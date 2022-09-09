e20425fad9b240c450cc7a13ae933da525236c36
aryan bramhane <aryanbramhane1@gmail.com>
2022-09-10 01:33:04 +0530

    deploying the first version

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Password Genrator
-pa
+[x] Pusblishing the first version of this app
+## Stretch Goals
+[ ] Ability to set password lenght
+[ ] Add "Copy-on-click" feature
+[ ] Toogle "symbols" and "numbers"on/off
--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
 
-let randomIndex = generateRandomNumber()
-let password = " "
-let passwordView = document.querySelector()
-
-function generateRandomNumber(){
-    let randomNumber =Math.floor( Math.random()* characters.length)
-    return randomNumber
+let passwordViewerOne = document.getElementById("password-view-one")
+let passwordViewerTwo = document.getElementById("password-view-two")
+function randomCharacter(){
+    let randomIndex = Math.floor(Math.random()* characters.length)
+    return randomIndex  
 }
 function passwordGenerator(){
-    for(let i = 0; i<=12;i++){
-        password += characters[generateRandomNumber()]
+    let randomPassword = ""
+    for (let i = 0; i < 12; i++) {
+        randomPassword += characters[randomCharacter()]
     }
-    return password
+    return randomPassword
 }
 
+function buttonClick(){
+    passwordViewerOne.textContent = passwordGenerator()
+    passwordViewerTwo.textContent = passwordGenerator()
+}
+
+
+
+// TODO
+//Add copy-to-click option
+//ability to set the password lenght
